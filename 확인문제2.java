c7e6ba22341af48121f81f48d63e764219853f48
kimsiiiilver <136664538+kimsiiiilver@users.noreply.github.com>
2023-06-29 17:09:58 +0900

    java 6/29

--- 확인문제2.java
+++ 확인문제2.java
@@ -1,17 +1,14 @@
 package sec02;
 
-import java.util.Scanner;
-
 public class 확인문제2 {
 
 	public static void main(String[] args) {
-		Scanner s = new Scanner(System.in);
-		System.out.println("근로시간을 입력하세요.");
-		int m = s.nextInt();
-		double result = (m >= 10) ? m * 9620 * 1.2 : m * 9620;
-		
-		System.out.println((int)result);
-
+		int sum = 0;
+		for (int i = 1; i <= 100; i++) {
+			if (i % 3 == 0) {
+				sum += i;
+			}
+		}
+		System.out.println("1~100까지 3의 배수의 합계 : " +sum);
 	}
-
 }
--- 확인문제3.java
+++ 확인문제3.java
@@ -1,23 +1,19 @@
 package sec02;
 
-import java.util.Scanner;
-
 public class 확인문제3 {
 
 	public static void main(String[] args) {
-		Scanner s = new Scanner(System.in);
-		System.out.println("학생 수를 입력하세요");
-		int st = s.nextInt();
-		System.out.println("연필 갯수를 입력하세요");
-		int p = s.nextInt();
+		for(int i=1; i<=4; i++) {
+			for(int j=1; j<=i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
 		
-		int a = p/st;
-		int b = p % st;
-		System.out.println("학생 한 명당 " + a + "개씩 연필을 가져가고 " + b + "개가 남아요");
+				
+			}
+			
 		
-
-		
-
 	}
 
-}
+
