8c330e19db5387c51c7115a4c2250eed60cc78ec
kimsiiiilver <136664538+kimsiiiilver@users.noreply.github.com>
2023-07-07 17:57:02 +0900

    java 7/7

--- Car.java
+++ Car.java
@@ -1,29 +1,42 @@
-package sec04;
+package sec05;
 
 public class Car {
 
-	private int speed = 100;
-
-	void powerOn() {
-		System.out.println("시동을 걸었습니다.");
-	}
-
-	void powerOff() {
-		System.out.println("시동을 끕니다.");
-	}
-
-	int speedUp(int x) {
-		speed+= x;
-		return speed;
-	}
+	// 필드 선언
+	String model;
+	int speed;
 	
-	int speedDown(int y) {
-		speed-= y;
-		return speed;
+	//기본생성자
+	Car(){
+		
 	}
 
-	int breakZero() {
-		speed= 0;
-		return speed;
+	// 매개변수 1개 생성자, 기본 생성자 x
+	Car(String model) {
+		this.model = model;
+	}
+
+	// 매개변수 1개 메소드, 리턴값 x
+	void setSpeed(int speed) {
+		this.speed = speed;
+	}
+
+	// 매개변수 없는 메소드, 리턴값x
+	void run() {
+		for (int i = 10; i <= 50; i += 10) {
+			this.setSpeed(i);
+			System.out.println(this.model + "가 달립니다.(시속 : " + this.speed + "km/h)");
+
+		}
+	}
+
+	public static void main(String[] args) {
+		Car myCar = new Car("포르쉐");
+		myCar.run();
+		
+		Car yourCar = new Car("제규어");
+		yourCar.speed=60;
+		
+		yourCar.run();
 	}
 }
